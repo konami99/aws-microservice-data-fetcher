7a58d30b5ed88651b0916d64af55e9e3f5040727
Richard Chou <konami99@hotmail.com>
2023-08-17 21:34:09 +0800

    get weather data

--- src/app.ts
+++ src/app.ts
@@ -1,20 +1,24 @@
 import { EventBridgeClient, ActivateEventSourceCommand, PutEventsCommandInput, PutEventsCommand } from "@aws-sdk/client-eventbridge";
+import axios from "axios";
 
-//AWS.config.update({region: 'us-west-2'});
-//const eventbridge = new AWS.EventBridge();
-
-const client = new EventBridgeClient({ region: 'us-west-2' });
+const client = new EventBridgeClient();
 
 export const lambdaHandler = async (event: any): Promise<any> => {
 	try {
 
+		const city = 'sydney';
+		const unit = 'metric';
+		const apiKey = 'e87991825328b10ee03463453976a4bb';
+
+		const weatherData = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=${unit}`);
+
+		const total = { ...weatherData.data["main"], time: new Date() };
+		console.log('weatherData:', JSON.stringify(total));
+
 		const entry = {
 			Entries: [ 
 				{
-					Detail: JSON.stringify({
-						"message": "Hello from publisher",
-						"state": "new"
-					}),
+					Detail: JSON.stringify(total),
 					DetailType: 'Message',
 					EventBusName: 'default',
 					Source: 'demo.event',
